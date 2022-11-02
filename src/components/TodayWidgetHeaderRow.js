702f372879c932f74cf48a021f65f001dc39a454
Bram van Rijswijk <b.vanrijswijk@outlook.com>
2022-11-02 15:43:15 +0100

    feat: added names to today widget row columns

--- src/components/TodayWidgetHeaderRow.js
+++ src/components/TodayWidgetHeaderRow.js
@@ -7,14 +7,15 @@ const TodayWidgetHeaderRow = props => {
   return (
       <div
           className="grid grid-cols-4 gap-8 bg-gradient-to-r from-slate-600 to-slate-700 px-8 py-1 items-center">
-        <p className="text-white"
-           style={{minWidth: '100px', maxWidth: '100px'}}>
+        <div data-column-name="time" className="text-white"
+             style={{minWidth: '100px', maxWidth: '100px'}}>
             <span
                 className="flex items-center justify-center bg-slate-800 font-semibold px-3 py-1">
                 nu
             </span>
-        </p>
-        <p className="text-white">
+        </div>
+
+        <div data-column-name="temperature" className="text-white">
           <span className="flex items-center space-x-1">
             <ForecastIcon icon={getIconNameById(
                 props.forecastData.current.weather[0].icon.substring(0,
@@ -22,18 +23,21 @@ const TodayWidgetHeaderRow = props => {
             <span className="-ml-4">{Math.round(
                 props.forecastData.current.temp)}°</span>
           </span>
-        </p>
-        <p className="flex-grow text-left text-white">
+        </div>
+
+        <div data-column-name="description"
+             className="flex-grow text-left text-white">
           {props.forecastData.current.weather[0].description}
-        </p>
-        <p className="text-white">
+        </div>
+
+        <div data-column-name="wind" className="text-white">
             <span className="flex items-center space-x-2">
               <RotatableArrow color={'#FFF'}
                               degrees={props.forecastData.current.wind_deg}/>
               <span>{calculateWindSpeedInBeaufort(
                   props.forecastData.current.wind_speed)} Bft</span>
             </span>
-        </p>
+        </div>
       </div>
   );
 };
--- src/components/TodayWidgetRow.js
+++ src/components/TodayWidgetRow.js
@@ -8,13 +8,15 @@ const TodayWidgetRow = props => {
   return (
       <div
           className="grid grid-cols-4 gap-8 bg-white px-8 py-1 items-center border-b">
-        <p style={{minWidth: '100px', maxWidth: '100px'}}>
+        <div data-column-name="time"
+             style={{minWidth: '100px', maxWidth: '100px'}}>
           <time
               className="flex items-center justify-center bg-gray-100 font-semibold px-3 py-1">
             {formatTime(props.hourlyData.dt)}
           </time>
-        </p>
-        <p>
+        </div>
+
+        <div data-column-name="temperature">
             <span className="flex items-center space-x-1">
               <ForecastIcon color={'#000'} icon={getIconNameById(
                   props.hourlyData.weather[0].icon.substring(0,
@@ -22,18 +24,20 @@ const TodayWidgetRow = props => {
               <span className="-ml-4">{Math.round(
                   props.hourlyData.temp)}°</span>
             </span>
-        </p>
-        <p className="flex-grow text-left">
-          {props.hourlyData.weather[0].description ?? ''}
-        </p>
-        <p>
+        </div>
+
+        <div data-column-name="description" className="flex-grow text-left">
+          {props.hourlyData.weather[0].description}
+        </div>
+
+        <div data-column-name="wind">
             <span className="flex items-center space-x-2">
               <RotatableArrow color={'#000'}
                               degrees={props.hourlyData.wind_deg}/>
               <span>{calculateWindSpeedInBeaufort(
                   props.hourlyData.wind_speed)} Bft</span>
             </span>
-        </p>
+        </div>
       </div>
   );
 };
