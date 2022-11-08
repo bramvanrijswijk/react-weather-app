8c51b1189189e375b2b65918f2084f8f80c3def2
Bram van Rijswijk <b.vanrijswijk@outlook.com>
2022-11-08 16:06:43 +0100

    feat: expand weather data interface

--- src/interfaces/weather-data.interface.ts
+++ src/interfaces/weather-data.interface.ts
@@ -1,5 +1,68 @@
+interface BasicWeather {
+  dt: number;
+  pressure: number;
+  humidity: number;
+  dew_point: number;
+  uvi: number;
+  clouds: number;
+  visibility: number;
+  wind_speed: number;
+  wind_deg: number;
+  wind_gust: number;
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+}
+
+export interface CurrentWeatherDetails extends BasicWeather {
+  sunrise: number;
+  sunset: number;
+  moonrise: number;
+  moonset: number;
+  moon_phase: number;
+  temp: number;
+  feels_like: number;
+}
+
+export interface HourlyWeatherDetails extends BasicWeather {
+  temp: number;
+  feels_like: number;
+  pop: number;
+  rain: {
+    '1h': number;
+  }
+}
+
+export interface DailyWeatherDetails extends BasicWeather {
+  sunrise: number;
+  sunset: number;
+  moonrise: number;
+  moonset: number;
+  moon_phase: number;
+  temp: {
+    day: number;
+    min: number;
+    max: number;
+    night: number;
+    eve: number;
+    morn: number;
+  }
+  feels_like: {
+    day: number;
+    night: number;
+    eve: number;
+    morn: number;
+  }
+  clouds: number;
+  pop: number;
+  rain: number;
+}
+
 export interface WeatherData {
-  daily: [];
-  current: [];
-  hourly: [];
+  current: CurrentWeatherDetails[];
+  hourly: HourlyWeatherDetails[];
+  daily: DailyWeatherDetails[];
 }
